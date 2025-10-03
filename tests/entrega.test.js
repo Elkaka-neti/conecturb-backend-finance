5ee5615ebfdb7900c41a44791471f29fc98bf1de
knf9090-hub <knf9090@gmail.com>
2025-10-03 11:15:19 +0000

    [UPDATE] Atualizando testes de rotas

--- tests/entrega.test.js
+++ tests/entrega.test.js
@@ -11,7 +11,7 @@ it('Deve retornar com sucesso a requisição e salva-la', async() => {
         id: 892382948,
         loja_id: 987654321,
         distance: 12,
-        horario: 'guygyugyu',
+        horario: '12:30 12/12/2023',
         valor_corrida: 3.49
       }
     })
@@ -20,4 +20,22 @@ it('Deve retornar com sucesso a requisição e salva-la', async() => {
     expect(res.body).toEqual({ mensagem: 'Entrega realizada com sucesso', status: 'ok'})
   });
 
-})
+  it('Deve retornar erro 400 para requisição com distancia negativa', async() => {
+    const res = await request(app)
+     .post('/entrega')
+     .send({
+   data: {
+       id: 892382948,
+       loja_id: 987654321,
+       distance: -12,
+       horario: '12:30 12/12/2023',
+       valor_corrida: 3.49
+     }
+   })
+     .set('Accept', 'application/json');
+   expect(res.statusCode).toBe(400);
+   expect(res.text).toBe('Requisição inválida: distância e valor da corrida devem ser positivos')
+ });
+
+ 
+  })
